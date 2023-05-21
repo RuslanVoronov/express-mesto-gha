f4795e1dc02af19b1ce71f9b24092e52999e53e9
Ruslan Voronov <voronov.rv.99@yandex.ru>
2023-05-22 02:54:32 +0300

    Добавление валидации схемы User

--- models/user.js
+++ models/user.js
@@ -5,23 +5,39 @@ const userSchema = new mongoose.Schema({
         type: String,
         minlength: 2,
         maxlength: 30,
-        default: 'Жак-Ив Кусто'
+        default: 'Жак-Ив Кусто',
+        validate: {
+            validator: ({ length }) => length >= 2 && length <= 30,
+            message: 'Имя пользователя должно быть длиной от 2 до 30 символов',
+        },
     },
     about: {
         type: String,
         minlength: 2,
         maxlength: 30,
-        default: 'Исследователь'
+        default: 'Исследователь',
+        validate: {
+            validator: ({ length }) => length >= 2 && length <= 30,
+            message: 'Имя пользователя должно быть длиной от 2 до 30 символов',
+        },
     },
     avatar: {
         type: String,
-        default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'
+        default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+        validate: {
+            validator: (email) => /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/.test(email),
+            message: 'Требуется ссылка на изображение',
+        },
     },
     email: {
         type: String,
         require: true,
-        unique: true
+        unique: true,
+        validate: {
+            validator: (email) => /.+@.+\..+/.test(email),
+            message: 'Требуется ввести электронный адрес',
         },
+    },
     password: {
         type: String,
         require: true
