e312bf3b52db94266d7798c1bab0ad33ed56d409
Ruslan Voronov <voronov.rv.99@yandex.ru>
2023-05-22 22:33:36 +0300

    modified:   routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,25 @@
 const { getUsers, getUserById, updateUser, updateAvatar, getCurrentUser, login } = require('../controllers/users');
 const { celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
+
 
 const router = require('express').Router();
 
 router.get('/', getUsers);
 
-router.patch('/me', updateUser);
+router.patch('/me', celebrate({
+    body: Joi.object().keys({
+        name: Joi.string().min(2).max(30),
+        about: Joi.string().min(2).max(30),
+        avatar: Joi.string().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/)
+    })
+}), updateUser);
 
 router.get('/me', getCurrentUser);
 
 router.get('/:id', getUserById);
 
 router.patch('/me/avatar', updateAvatar)
+router.use(errors())
 
 module.exports = router;
