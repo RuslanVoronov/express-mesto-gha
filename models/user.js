ed1c6f9c9911c10eea2991ae02729d80144a6d56
Ruslan Voronov <voronov.rv.99@yandex.ru>
2023-05-23 05:53:27 +0300

    Исправление ошибок после 1 ревью

--- models/user.js
+++ models/user.js
@@ -1,79 +1,62 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 2,
-        maxlength: 30,
-        default: 'Жак-Ив Кусто',
-        // validate: {
-        //     validator: ({ length }) => length >= 2 && length <= 30,
-        //     message: 'Имя пользователя должно быть длиной от 2 до 30 символов',
-        // },
-    },
-    about: {
-        type: String,
-        minlength: 2,
-        maxlength: 30,
-        default: 'Исследователь',
-        // validate: {
-        //     validator: ({ length }) => length >= 2 && length <= 30,
-        //     message: 'Имя пользователя должно быть длиной от 2 до 30 символов',
-        // },
-    },
-    avatar: {
-        type: String,
-        default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-        validate: {
-            validator: (string) => {
-                validator.isURL(string);
-            },
-        },
-        // validate: {
-        //     validator: (email) => /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/.test(email),
-        //     message: 'Требуется ссылка на изображение',
-        // },
-    },
-    email: {
-        type: String,
-        require: true,
-        unique: true,
-        validate: {
-            validator: (string) => {
-                validator.isEmail(string);
-            },
-        },
-        // validate: {
-        //     validator: (email) => /.+@.+\..+/.test(email),
-        //     message: 'Требуется ввести электронный адрес',
-        // },
-    },
-    password: {
-        type: String,
-        require: true,
-        select: false,
-    }
+	name: {
+		type: String,
+		minlength: 2,
+		maxlength: 30,
+		default: "Жак-Ив Кусто",
+	},
+	about: {
+		type: String,
+		minlength: 2,
+		maxlength: 30,
+		default: "Исследователь",
+	},
+	avatar: {
+		type: String,
+		default: "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
+		validate: {
+			validator: (string) => {
+				validator.isURL(string);
+			},
+		},
+	},
+	email: {
+		type: String,
+		require: true,
+		unique: true,
+		validate: {
+			validator: (string) => {
+				validator.isEmail(string);
+			},
+		},
+	},
+	password: {
+		type: String,
+		require: true,
+		select: false,
+	}
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-    return this.findOne({ email }).select('+password')
-        .then((user) => {
-            console.log(user)
-            if (!user) {
-                return Promise.reject(new Error('Неправильные почта или пароль'));
-            }
-            return bcrypt.compare(password, user.password)
-                .then((matched) => {
+	return this.findOne({ email }).select("+password")
+		.then((user) => {
+			if (!user) {
+				return Promise.reject(new Error("Неправильные почта или пароль"));
+			}
+			return bcrypt.compare(password, user.password)
+				.then((matched) => {
 
-                    if (!matched) {
-                        return Promise.reject(new Error('Неправильные почта или пароль'));
-                    }
+					if (!matched) {
+						return Promise.reject(new Error("Неправильные почта или пароль"));
+					}
 
-                    return user; // теперь user доступен
-                });
-        });
+					return user; // теперь user доступен
+				});
+		});
 };
 
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model("user", userSchema);
